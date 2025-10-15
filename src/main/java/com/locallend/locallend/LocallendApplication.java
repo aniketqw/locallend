3e5e6d41d9f38875add30e3cf876433d53d16f40
aniketqw <aniket21stcentury@gmail.com>
2025-10-16 04:53:31 +0530

    first commit

---
+++ src/main/java/com/locallend/locallend/LocallendApplication.java
@@ -0,0 +1,13 @@
+package com.locallend.locallend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocallendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocallendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/locallend/locallend/LocallendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.locallend.locallend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocallendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
