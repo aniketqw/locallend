a3e31e0ac228bc693f00e8790c1008b3e914ee32
Prishiv Singh Jamwal <prishiv123@hotmail.com>
2025-11-14 13:03:37 +0530

    attempt at hosting?

--- src/main/java/com/locallend/locallend/config/CorsConfig.java
+++ src/main/java/com/locallend/locallend/config/CorsConfig.java
@@ -31,7 +31,8 @@ public class CorsConfig {
             "http://localhost:3000",     // Create React App default
             "http://localhost:5173",     // Vite default port
             "http://127.0.0.1:3000",     // Alternative localhost
-            "http://127.0.0.1:5173"      // Alternative localhost
+            "http://127.0.0.1:5173",
+            "https://v0-community-sharing-platform.vercel.app"// Alternative localhost
         ));
         
         // Allow specific HTTP methods
